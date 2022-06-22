d52ff66e7d485a78a14ecee31c06abf21a88e5de
Daniil <coolswood@yandex.ru>
2022-06-22 09:42:03 +0300

    update

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
-import fastify from 'fastify';
+import fastify, {FastifyRequest} from 'fastify';
 import cookie, {FastifyCookieOptions} from '@fastify/cookie';
 import cors from '@fastify/cors';
+import websocket from '@fastify/websocket';
 import {initDb} from "./db";
 
 const app = fastify({logger: true});
@@ -8,14 +9,90 @@ const app = fastify({logger: true});
 const hours_3 = 1000 * 60 * 60 * 3;
 
 app.register(cookie, {
-    secret: "my-secret",
-    parseOptions: {}
+    parseOptions: {
+        sameSite: true,
+        path: '/',
+    }
 } as FastifyCookieOptions)
+
+app.register(websocket)
 
 app.register(cors, {
     origin: "*",
-    methods: ["GET", "POST", "PUT"]
+    methods: ["GET", "POST", "PUT"],
 });
+
+app.register(async function (fastify) {
+    fastify.get('/ws', {websocket: true}, (connection /* SocketStream */, req: FastifyRequest<{
+        Querystring: { user: string }
+    }>) => {
+        connection.socket.on('message', async (m: Buffer) => {
+            const message = JSON.parse(m.toString());
+
+            const db = await initDb();
+
+            if (message.event === 'getTickets') {
+                const user = req.query.user;
+                const data = await db('userData').select('*');
+
+                const tickets = data.reduce<api.getTickets.response>((acc, i) => {
+                    acc[i.id] = {
+                        text: i.text,
+                        screenY: i.screenY,
+                        screenX: i.screenX,
+                        canEdit: i.user === user,
+                        user: i.user !== user ? user : undefined
+                    }
+
+                    return acc;
+                }, {});
+
+                connection.socket.send(JSON.stringify(tickets));
+            }
+
+            if(message.event === 'create') {
+                const user = req.query.user;
+                const {id, screenY, screenX} = message.data;
+
+                await db('userData').insert({
+                    id, screenY, screenX, user
+                });
+            }
+
+            if(message.event === 'update') {
+                const {id, text, screenY, screenX} = message.data;
+
+                await db('userData').where({id}).update({
+                    text, screenY, screenX
+                });
+            }
+
+            const user = req.query.user;
+
+            const data = await db('userData').select('*');
+
+            const tickets = data.reduce<api.getTickets.response>((acc, i) => {
+                acc[i.id] = {
+                    text: i.text,
+                    screenY: i.screenY,
+                    screenX: i.screenX,
+                    canEdit: i.user === user,
+                    user: i.user !== user ? user : undefined
+                }
+
+                return acc;
+            }, {});
+
+            const allTickets = JSON.stringify(tickets);
+
+            fastify.websocketServer.clients.forEach(async function each(client: any) {
+                if (client.readyState === 1) {
+                    connection.socket.send(allTickets);
+                }
+            })
+        })
+    })
+})
 
 app.put<{ Body: { user: string }; Reply: {} }>("/api/create", async (request, reply) => {
     const user = request.body.user;
@@ -26,7 +103,7 @@ app.put<{ Body: { user: string }; Reply: {} }>("/api/create", async (request, re
 
     const data = await db('userName').select(['user']);
 
-    reply.cookie('user', user, {maxAge: hours_3});
+    reply.setCookie('user', user, {maxAge: hours_3});
 
     return true;
 });
@@ -34,7 +111,7 @@ app.put<{ Body: { user: string }; Reply: {} }>("/api/create", async (request, re
 app.get<{ Body: api.getTickets.request; Reply: api.getTickets.response }>("/api/tickets", async (request, reply) => {
     const user = request.cookies.user;
 
-    if(user == undefined) {
+    if (user == undefined) {
         return reply.status(401).send();
     }
 
@@ -54,31 +131,6 @@ app.get<{ Body: api.getTickets.request; Reply: api.getTickets.response }>("/api/
 
         return acc;
     }, {}));
-});
-
-app.put<{ Body: api.putTicket.request; Reply: api.putTicket.response }>("/api/ticket", async (request, reply) => {
-    const user = request.cookies.user;
-    const {id, text, screenY, screenX} = request.body;
-
-    const db = await initDb()
-
-    await db('userData').insert({
-        id, text, screenY, screenX, user
-    });
-
-    return reply.send({});
-});
-
-app.patch<{ Body: api.patchTicket.request; Reply: api.patchTicket.response }>("/api/ticket", async (request, reply) => {
-    const {id, text, screenY, screenX} = request.body;
-
-    const db = await initDb()
-
-    await db('userData').where({id}).update({
-        text, screenY, screenX
-    });
-
-    return reply.send({});
 });
 
 (async () => {
--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,49 +1,47 @@
 import update from 'immutability-helper'
-import type {CSSProperties, FC} from 'react'
+import type {FC} from 'react'
 import {useEffect, useRef} from 'react'
-import {createRef} from 'react'
-import {useCallback, useState} from 'react'
+import {useState} from 'react'
 import {useDrop} from 'react-dnd'
+import useWebSocket from 'react-use-websocket'
 import {boxSize, ItemTypes, placeholder} from 'src/constants'
 import {ApiRequest, getRandomInt} from 'src/helper'
 import type {DragItem} from 'src/types'
-
-import {DraggableBox} from '../DraggableBox/DraggableBox'
+import { DraggableBox } from '../DraggableBox/DraggableBox'
+import cookie from 'js-cookie';
 
 import styles from './styles.module.scss'
 
 type BoxMap = api.getTickets.response;
 
+const socketUrl = 'ws://127.0.0.1:3001/ws';
+
 export const Container: FC = () => {
-    const [isLoaded, setIsLoaded] = useState(false);
     const [boxes, setBoxes] = useState<BoxMap>({});
 
-    const authUser = () => {
-        const result = prompt('Fill your name');
-
-        ApiRequest('create', {user: result}, 'PUT').then(() => setIsLoaded(true));
-    }
-
-    useEffect(() => {
-            ApiRequest<api.getTickets.response, api.getTickets.request>('tickets', {}, 'GET').then((response) => {
-                setBoxes(response);
-                setIsLoaded(true);
-            }).catch(() => {
-                authUser();
-            })
-    }, [isLoaded])
+    const {
+        sendJsonMessage,
+    } = useWebSocket(socketUrl, {
+        queryParams: {user: cookie.get('user')!},
+        onOpen: () => {
+            sendJsonMessage({event: 'getTickets'});
+        },
+        onMessage: (message) => {
+            console.log(message.data)
+            setBoxes(JSON.parse(message.data));
+        },
+        onError: (error) => {
+            console.log('error', error);
+        },
+        shouldReconnect: (closeEvent) => true,
+    });
 
     const createNewBox = (e: React.MouseEvent<HTMLElement>) => {
         const id = getRandomInt();
         const screenY = e.screenY - boxSize.height / 2;
         const screenX = e.screenX - boxSize.width / 2;
 
-        ApiRequest<api.putTicket.response[], api.putTicket.request>('ticket', {
-            id,
-            text: placeholder,
-            screenY: screenY,
-            screenX: screenX
-        }, 'PUT');
+        sendJsonMessage({event: 'create', data: {id, screenX, screenY}});
 
         setBoxes(
             {
@@ -60,11 +58,8 @@ export const Container: FC = () => {
     }
 
     const moveBox = (id: string, screenX: number, screenY: number) => {
-        ApiRequest<api.patchTicket.response, api.patchTicket.request>('ticket', {
-            id,
-            screenX,
-            screenY
-        }, 'PATCH');
+
+        sendJsonMessage({event: 'update', data: {id, screenX, screenY}});
 
             setBoxes(
                 update(boxes, {
@@ -76,10 +71,8 @@ export const Container: FC = () => {
         }
 
         const onUpdateText = (id: string, text: string) => {
-            ApiRequest<api.patchTicket.response, api.patchTicket.request>('ticket', {
-                id,
-                text
-            }, 'PATCH');
+
+            sendJsonMessage({event: 'update', data: {id, text}});
 
             setBoxes(
                 update(boxes, {
@@ -108,8 +101,6 @@ export const Container: FC = () => {
         }),
         [moveBox],
     )
-
-    if(!isLoaded) return null;
 
     return (
         <div ref={drop} style={{
